00aeb24f1737ea38b25a383a0a81481ccc1aa3ab
sigavpn <34906324+sigavpn@users.noreply.github.com>
2018-08-27 09:53:35 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=
+1, shrink-to-fit=no">
+    <meta name="description" content="SigaVPN is a free no-log VPN th
+at allows torrenting">
+    <meta name="author" content="">
+    <meta name="abuseipdb-verification" content="lSpBaw42" />
+    <title>SigaVPN</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://sigavpn.com/vendor/bootstrap/css/bootstrap.mi
+n.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://sigavpn.com/css/modern-business.css" rel="styl
+esheet">
+<style>
+
+.card {
+        margin: 0 auto; /* Added */
+        float: none; /* Added */
+        margin-bottom: 10px; /* Added */
+}</style>
+  </head>
+
+  <body>
+<div class="container"><div class="row">
+<div class="card" class="mx-auto">
+<p class="card-header"><b>Netherlands</b> | Server load: <?php echo f
+ile_get_contents("http://51.15.2.172/bw.txt"); ?></p><div class="card
+-body">
+  <img class="card-img-top" src="http://51.15.2.172/stats.jpg">
+  <div class="card-block">
+  </div></div></div>
+
+</div>
+
+</div>
+
+
+<div class="container"><div class="row">
+<div class="card" class="mx-auto">
+<p class="card-header"><b>Latvia</b> | Server load: <?php echo file_g
+et_contents("http://185.61.149.116/bw.txt"); ?></p><div class="card-b
+ody">
+  <img class="card-img-top" src="http://185.61.149.116/stats.jpg">
+  <div class="card-block">
+  </div></div></div>
+
+</div>
+
+</div>
+
+
+
+<div class="container"><div class="row">
+<div class="card" class="mx-auto">
+<p class="card-header"><b>Sweden</b> | Server load: <?php echo file_g
+et_contents("http://95.215.44.147/bw.txt"); ?></p><div class="card-bo
+dy">
+  <img class="card-img-top" src="http://95.215.44.147/stats.jpg">
+  <div class="card-block">
+  </div></div></div>
+
+</div>
+
+</div>
+
+<div class="container"><div class="row">
+<div class="card" class="mx-auto">
+<p class="card-header"><b>Romania</b> | Server load: <?php echo file_
+get_contents("http://185.225.17.101/bw.txt"); ?></p><div class="card-
+body">
+  <img class="card-img-top" src="http://185.225.17.101/stats.jpg">
+  <div class="card-block">
+  </div></div></div>
+
+</div>
+
+</div>
+
+<div class="container"><div class="row">
+<div class="card" class="mx-auto">
+<p class="card-header"><b>Las Vegas</b> | Server load: <?php echo fil
+e_get_contents("http://205.185.122.55/bw.txt"); ?></p><div class="car
+d-body">
+  <img class="card-img-top" src="http://205.185.122.55/stats.jpg">
+  <div class="card-block">
+  </div></div></div>
+
+</div>
+
+</div>
+
+
+<div class="container"><div class="row">
+<div class="card" class="mx-auto">
+<p class="card-header"><b>Chicago</b> | Server load: <?php echo file_
+get_contents("http://38.128.66.208/bw.txt"); ?></p><div class="card-b
+ody">
+  <img class="card-img-top" src="http://38.128.66.208/stats.jpg">
+  <div class="card-block">
+  </div></div></div>
+
+</div>
+
+</div>
+
+<div class="container"><div class="row">
+<div class="card" class="mx-auto">
+<p class="card-header"><b>Canada</b> | Server load: <?php echo file_g
+et_contents("http://142.44.133.83/bw.txt"); ?></p><div class="card-bo
+dy">
+  <img class="card-img-top" src="http://142.44.133.83/stats.jpg">
+  <div class="card-block">
+  </div></div></div>
+
+
+</div></div>
+
+<footer><div class="container"><div class="row"><p class="mx-auto">Th
+anks to Torro for making this possible!</p></div></div></footer>
+  </body>
+
+</html>
